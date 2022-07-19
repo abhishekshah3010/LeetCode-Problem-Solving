5fa77a67255b54b41ed325b96d08de4d62eef97f
Abhishek Shah <63114437+abhishekshah3010@users.noreply.github.com>
2022-07-19 11:08:02 -0400

    Time: 631 ms (56.25%), Space: 25.8 MB (93.27%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -7,11 +7,17 @@ class Solution:
         #     s.add(nums[i])
         # return False
         
-        s = set()
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
-        return False
+        # s = set()
+        # for num in nums:
+        #     if num in s:
+        #         return True
+        #     s.add(num)
+        # return False
         
+        d = {}
+        for num in nums:
+            if num in d:
+                return True
+            d[num] = 1
+        return False
         
