a4b3b39a94757fa25acd3ef538fed8a3d5837861
Abhishek Shah <63114437+abhishekshah3010@users.noreply.github.com>
2022-07-29 01:51:22 -0400

    Time: 87 ms (68.42%), Space: 14.1 MB (13.34%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,22 +1,35 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        curr = set()
+#         curr = set()
+#         left = 0
+#         right = 0
+        
+#         # if len(s) == 0:
+#         #     return 0
+        
+#         res = float("-inf")
+        
+#         while right < len(s):
+#             if s[right] in curr:
+#                 curr.remove(s[left])
+#                 left += 1
+                
+#             elif s[right] not in curr:
+#                 curr.add(s[right])
+#                 res = max(res, right - left + 1)
+#                 right += 1
+        
+#         return res if len(s) != 0 else 0
+
+        charSet = set()
         left = 0
-        right = 0
+        ans  = 0
         
-        # if len(s) == 0:
-        #     return 0
-        
-        res = float("-inf")
-        
-        while right < len(s):
-            if s[right] in curr:
-                curr.remove(s[left])
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
                 left += 1
-            elif s[right] not in curr:
-                curr.add(s[right])
-                res = max(res, right - left + 1)
-                right += 1
-        
-        return res if len(s) != 0 else 0
+            charSet.add(s[right])
+            ans = max(ans, right - left + 1)
+        return ans
         
